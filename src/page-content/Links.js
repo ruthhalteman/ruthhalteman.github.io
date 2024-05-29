831756c8c21df739267da95c28e8c9ce7ba83863
Ruth <ruth@Ruths-MBP-2.localdomain>
2024-05-29 17:57:57 -0400

    Updates

--- src/page-content/Links.js
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export const Links = () => {
-  return <div>haaaaa</div>;
-};
--- src/page-content/utils.js
+++
@@ -1,6 +0,0 @@
-export const getYardage = (strips, width) => {
-  const yardage = (strips * width) / 36;
-  return yardage < 0.25
-    ? `${strips * width}" long`
-    : `${yardage.toString().slice(0, 4)} yard${yardage > 1 ? "s" : ""}`;
-};
